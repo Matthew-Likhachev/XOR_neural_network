e671d79823c2deca115982d74462b4fb38a87793
Dead Morose <89824983744@mail.ru>
2022-03-13 21:43:35 +0500

    added comments

--- SOR.py
+++ SOR.py
@@ -5,41 +5,50 @@ from keras import models
 from keras import layers
 import matplotlib .pyplot as plt
 
+#reform to np arrays necessarily
 x_train=np.array([[0,0], [0,1], [1,0], [1,1]])
 y_train=np.array([[0], [0], [1], [0]])
 
-#print(x_train[0])
+#for test
 x_test=np.array([[0,0], [0,1], [1,0], [1,1]])
 y_test=np.array([[0], [0], [1], [0]])
 
 
-
+#special class Sequential
 model = models.Sequential()
 model.add(layers.Dense(8,activation='tanh'))
 #model.add(layers.Dense(8, activation = 'relu'))
 model.add(layers.Dense(1, activation = 'sigmoid'))
 
-model.compile(optimizer='rmsprop',
+
+
+model.compile(optimizer='rmsprop',        #compiling model
     loss='binary_crossentropy',
-    metrics=['accuracy'])
+    metrics=['accuracy'])                   #for history model and graph(chart) not neccessary parametr
+
+
 
 history = model.fit(x_train,
   y_train,
   epochs=200,
-  batch_size=1,
-  validation_data=(x_test, y_test))
+  batch_size=1,                     #size of tensor in one epoch
+  validation_data=(x_test, y_test)) #history to validate, not neccessary
+
+
 
 history_dict = history.history
 #print(history_dict.keys())
 #>>dict_keys(['loss', 'accuracy', 'val_loss', 'val_accuracy
 
+
+#for graph (chart)
 loss_values = history_dict['loss']
 val_loss_values = history_dict['val_loss']
-
 epochs = range(1, len(history_dict['accuracy'])+ 1)
 
-#plt.plot( )
 
+
+#lib for graph(chart)
 plt.plot(epochs, loss_values, 'bo', label='Training loss')
 plt.plot(epochs, val_loss_values, 'b', label='Validation loss') 
 plt.title('Training and validation loss')
@@ -49,7 +58,7 @@ plt.legend()
 plt.show()
 
 
-plt.clf() #Очистить рисунок
+plt.clf() #clear graph
 acc_values = history_dict['accuracy']
 val_acc_values = history_dict['val_accuracy']
 plt.plot(epochs, history_dict['accuracy'], 'bo', label='Training acc')
